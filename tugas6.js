766c791dd1c4a45512b7f193e65522ae12e1181a
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-02 14:01:26 +0700

    Tugas 6 JS
    Tugas Niomic

---
+++ tugas6.js
@@ -0,0 +1,15 @@
+
+const loop = () => {
+    for(i=1;i<=20;i++){
+        if(i%2==0){
+            console.log('bilangan genap')
+        }else{
+            console.log(i)
+        }
+        
+    }
+
+}
+
+loop();
+
